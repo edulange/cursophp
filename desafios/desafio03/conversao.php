baff3b6a57ace2e73251d0020afe6a3ee9adc285
Eduardo Lange <eduardoandrelange@gmail.com>
2024-10-20 11:09:52 -0300

    ajustando o ex3

--- desafios/desafio03/conversao.php
+++ desafios/desafio03/conversao.php
@@ -17,13 +17,26 @@
     <body>
 
         <?php 
+    //variável do cliente
     $moeda = $_GET["moeda"] ?? "sem moeda";
+    
+    //Cotação
     const COTACAO = 5.69;
     $moedaFloat = floatval($moeda); // Converte para um valor numérico
-    $conversao = $moedaFloat * COTACAO;
-    echo "Seus <i>$moeda</i> equivalem a <strong>U$ $conversao</strong>";
+    //converão
+    $conversao = $moedaFloat / COTACAO;
+
+    //formatação de moedas com internacionalização
+    //Biblioteca intl (Internallization PHP)
+    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+    
+    
+    echo "Seus " . numfmt_format_currency($padrao, $moeda, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $conversao, "USD");
+    //jeito fácil echo "Seus R\$ " . number_format($moeda, 2, ",", ".") . " equivalem a U\$" . $conversao;
     echo "<br><strong>* Cotação fixa de R$ " . COTACAO . " </strong>informada diretamente no código."
     ?>
+
+
         <button><a href="javascript:history.go(-1)">Voltar</a></button>
 </main>
 </body>
